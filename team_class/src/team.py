41a741eade510218ca4614dafb47fffbcb2f4374
Ben <bjporter123@gmail.com>
2022-11-28 16:57:53 +0000

    lab with vicotira

---
+++ team_class/src/team.py
@@ -0,0 +1,21 @@
+class Team():
+    def __init__(self, input_team_name, input_list_of_players, input_coach_name):
+        self.name = input_team_name
+        self.players = input_list_of_players
+        self.coach = input_coach_name
+        self.points = 0
+    
+    def add_player(self, new_player):
+        self.players.append(new_player)
+    
+    def has_player(self, player_name):
+        for name in self.players:
+            if name == player_name:
+                return True 
+        return False
+
+    def play_game(self):
+        if True:
+           points += 3
+        return points 
+    
